6a866d2fd8f54deac42c86eeeefefb7257da598d
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-11-28 13:48:21 -0500

    fix(extract location file)

---
+++ src/lib/locations.ts
@@ -0,0 +1,199 @@
+export const locations = {
+    'Amazonas': {
+      'Chachapoyas': ['Chachapoyas', 'Asunción', 'Balsas', 'Cheto', 'Chiliquín', 
+                      'Chuquibamba', 'Granada', 'Huancas', 'La Jalca', 
+                      'Leimebamba', 'Levanto', 'Magdalena', 'Mariscal Castilla', 
+                      'Molinopampa', 'Montevideo', 'Olleros', 'Quinjalca', 
+                      'San Francisco de Daguas', 'San Isidro de Maino', 
+                      'Soloco', 'Sonche'],
+      'Bagua': ['Bagua', 'Aramango', 'Copallín', 'El Parco', 'Imaza', 'La Peca'],
+      'Bongará': ['Jumbilla', 'Chisquilla', 'Churuja', 'Corosha', 'Cuispes', 
+                  'Florida', 'Jazán', 'Recta', 'San Carlos', 
+                  'Shipasbamba', 'Valera', 'Yambrasbamba']
+    },
+    'Áncash': {
+      'Huaraz': ['Huaraz', 'Cochabamba', 'Colcabamba', 'Huanchay', 
+                 'Independencia', 'Jangas', 'La Libertad', 'Olleros', 
+                 'Pampas Grande', 'Pariacoto', 'Pira', 'Tarica'],
+      'Carhuaz': ['Carhuaz', 'Acopampa', 'Amashca', 'Anta', 'Ataquero', 
+                  'Marcara', 'Pariahuanca', 'San Miguel de Aco', 
+                  'Shilla', 'Tinco', 'Yungar']
+    },
+    'Apurímac': {
+      'Abancay': ['Abancay', 'Chacoche', 'Circa', 'Curahuasi', 'Huanipaca', 
+                  'Lambrama', 'Pichirhua', 'San Pedro de Cachora', 
+                  'Tamburco'],
+      'Andahuaylas': ['Andahuaylas', 'Andarapa', 'Chiara', 'Huancarama', 
+                      'Huancaray', 'Huayana', 'Kaquiabamba', 
+                      'Kishuara', 'Pacobamba', 'Pacucha', 
+                      'Pampachiri', 'Pocohuanca', 'San Antonio de Cachi', 
+                      'San Jerónimo', 'Talavera', 'Tumay Huaraca', 
+                      'Turpo']
+    },
+    'Arequipa': {
+      'Arequipa': ['Alto Selva Alegre', 'Arequipa', 'Cayma', 
+                   'Cerro Colorado', 'Characato', 'Chiguata', 
+                   'Jacobo Hunter', 'José Luis Bustamante y Rivero', 
+                   'La Joya', 'Mariano Melgar', 'Miraflores', 
+                   'Mollebaya', 'Paucarpata', 'Pocsi', 
+                   'Polobaya', 'Quequeña', 'Sabandia', 
+                   'Sachaca', 'San Juan de Siguas', 
+                   'San Juan de Tarucani', 'Santa Isabel de Siguas', 
+                   'Santa Rita de Siguas', 'Socabaya', 
+                   'Tiabaya', 'Uchumayo', 'Vitor', 
+                   'Yanahuara', 'Yarabamba', 'Yura'],
+      'Camana': ['Camaná', 'José María Quimper', 'Mariano Nicolás Valcárcel', 
+                 'Mariscal Cáceres', 'Nicolás de Piérola', 'Ocoña', 
+                 'Quilca', 'Samuel Pastor'],
+      'Islay': ['Cocachacra', 'Dean Valdivia', 'Islay', 
+                'Mejía', 'Mollendo', 'Punta de Bombón']
+    },
+    'Ayacucho': {
+      'Ayacucho': ['Ayacucho', 'Acocro', 'Carmen Alto', 'Chiara', 
+                   'Huamanguilla', 'Jesús Nazareno', 'Pueblo Nuevo', 
+                   'Quinua', 'San José de Ticllas', 'San Juan Bautista'],
+      'Cangallo': ['Cangallo', 'Andamarca', 'Ayahuanca', 'Chuschi', 
+                   'Huancaraylla', 'Mollepampa', 'Sancos'],
+      'Huanta': ['Huanta', 'Ayahuanca', 'Huamanguilla', 'San Juan de Chacña']
+    },
+    'Cajamarca': {
+      'Cajamarca': ['Cajamarca', 'Baños del Inca', 'Chetilla', 
+                    'Encañada', 'Jesús', 'Los Baños del Inca', 
+                    'Magdalena', 'Matara', 'Pampa Hermosa'],
+      'Celendín': ['Celendín', 'Chumuch', 'La Unión', 
+                   'Leimebamba', 'Mollepampa', 'Pachacutec'],
+      'Chota': ['Chota', 'Chalamarca', 'Condebamba', 'Huambos', 
+                'Llama', 'Miracosta', 'Tocmoche']
+    },
+    'Cusco': {
+      'Cusco': ['Cusco', 'San Sebastián', 'San Jerónimo', 
+                'Santiago', 'Wanchaq'],
+      'Urubamba': ['Chinchero', 'Huayllabamba', 'Maras', 
+                   'Ollantaytambo', 'Urubamba', 'Yucay'],
+      'Anta': ['Anta', 'Cachimayo', 'Chinchaypujio', 
+               'Huarocondo', 'Limatambo', 'Mollepata', 
+               'Pucyura', 'Zurite']
+    },
+    'Huancavelica': {
+      'Huancavelica': ['Huancavelica', 'Acobamba', 'Angaraes'],
+      'Castrovirreyna': ['Castrovirreyna', 'Arma'],
+      'Churcampa': ['Churcampa', 'Anco'],
+    },
+    'Huánuco': {
+      'Huánuco': ['Huánuco', 'Chinchao', 'San Francisco de Cayrán'],
+      'Marañón': ['Marañón', 'Pachachaca'],
+      'Leoncio Prado': ['Leoncio Prado', 'Rupa-Rupa']
+    },
+    'Ica': {
+      'Ica': ['Ica', 'Chincha Alta', 'Pisco', 'Nazca'],
+      'Palpa': ['Palpa', 'Tonosí'],
+      'Nasca': ['Nasca', 'Marcona']
+    },
+    'Junín': {
+      'Huancayo': ['Huancayo', 'Chilca', 'El Tambo', 
+                   'Pilcomayo', 'Pucará', 'Quilcas', 
+                   'San Agustín', 'Sapallanga', 'Sicaya'],
+      'Chanchamayo': ['Chanchamayo', 'Perené', 'Pichanaqui', 
+                      'San Luis de Shuaro', 'San Ramón', 
+                      'Vitoc']
+    },
+    'La Libertad': {
+      'Trujillo': ['Trujillo', 'El Porvenir', 'La Esperanza', 
+                   'Moche', 'Salaverry', 'Víctor Larco', 
+                   'Laredo', 'FLORENCIA DE MORA', 'Sinsicap'],
+      'Pacasmayo': ['Pacasmayo', 'Jequetepeque', 'San Pedro de Lloc'],
+      'Ascope': ['Ascope', 'Chicama', 'Paiján']
+    },
+    'Lambayeque': {
+      'Chiclayo': ['Chiclayo', 'Chongoyape', 'Eten', 'Eten Puerto', 
+                   'José Leonardo Ortiz', 'La Victoria', 'Lambayeque', 
+                   'Monsefu', 'Nueva Arica', 'Olmos', 'Pomalca', 
+                   'Pucalá', 'Reque', 'Salas', 'San José', 'Tuman'],
+      'Ferreñafe': ['Ferreñafe', 'Cañaris', 'Incahuasi', 'Pitipo', 'Pueblo Nuevo'],
+      'Lambayeque': ['Chongoyape', 'Eten', 'Eten Puerto', 'José Leonardo Ortiz', 
+                     'La Victoria', 'Lambayeque', 'Monsefu', 'Nueva Arica']
+    },
+    'Lima': {
+      'Lima': ['Ancón', 'Ate', 'Barranco', 'Breña', 'Carabayllo', 
+               'Chaclacayo', 'Chorrillos', 'Cieneguilla', 'Comas', 
+               'El Agustino', 'Independencia', 'Jesús María', 
+               'La Molina', 'La Victoria', 'Lince', 'Los Olivos', 
+               'Lurigancho-Chosica', 'Lurin', 'Magdalena del Mar', 
+               'Miraflores', 'Pachacamac', 'Pucusana', 'Pueblo Libre', 
+               'Puente Piedra', 'Punta Hermosa', 'Punta Negra', 
+               'Rímac', 'San Bartolo', 'San Borja', 'San Isidro', 
+               'San Juan de Lurigancho', 'San Juan de Miraflores', 
+               'San Luis', 'San Martín de Porres', 'San Miguel', 
+               'Santa Anita', 'Santa María del Mar', 'Santa Rosa', 
+               'Santiago de Surco', 'Surquillo', 'Villa El Salvador', 
+               'Villa María del Triunfo'],
+      'Callao': ['Callao', 'Bellavista', 'Carmen de la Legua-Reynoso', 
+                 'La Perla', 'La Punta', 'Ventanilla', 'Mi Perú']
+    },
+    'Loreto': {
+      'Iquitos': ['Iquitos', 'Belen', 'San Juan Bautista', 
+                  'Punta de San Juan', 'San Pablo', 'Indiana', 
+                  'Nauta', 'Requena', 'Contamana'],
+      'Alto Amazonas': ['Alto Amazonas', 'Pebas', 'Yurimaguas'],
+      'Maynas': ['Maynas', 'Nauta', 'San Juan de Lurigancho']
+    },
+    'Madre de Dios': {
+      'Puerto Maldonado': ['Puerto Maldonado', 'Inambari', 'Las Piedras', 
+                            'Manu', 'Tambopata'],
+      'Tambopata': ['Tambopata', 'Puerto Maldonado']
+    },
+    'Moquegua': {
+      'Moquegua': ['Moquegua', 'Carumas', 'Cuchumbaya', 'San Cristóbal', 
+                   'San Fernando', 'Santo Domingo', 'Torata'],
+      'Ilo': ['Ilo', 'El Algarrobal', 'Pachía', 'Puquina', 'Viñani']
+    },
+    'Pasco': {
+      'Pasco': ['Pasco', 'Chaupimarca', 'Huasahuasi', 'Paucartambo', 
+                'Yanahuanca', 'Ticlacayán', 'Tumbo', 'Paucartambo'],
+      'Oxapampa': ['Oxapampa', 'Villa Rica', 'Chontabamba', 'Palcazu', 
+                   'Vraem', 'Tinki', 'Atalaya']
+    },
+    'Piura': {
+      'Piura': ['Piura', 'Castilla', 'Catacaos', 'Cura Mori', 'La Arena', 
+                'La Unión', 'Las Lomas', 'Tambo Grande', 'Veintiséis de Octubre'],
+      'Sullana': ['Sullana', 'Bellavista', 'Ignacio Escudero', 
+                  'Lancones', 'Marcavelica', 'Salitral'],
+      'Talara': ['Talara', 'El Alto', 'La Brea', 'Los Órganos', 'Máncora'],
+      'Paita': ['Paita', 'Colán', 'Vichayal', 'Amotape']
+    },
+    'Puno': {
+      'Puno': ['Puno', 'Acora', 'Arapa', 'Atuncolla', 'Chucuito', 'Desaguadero', 
+               'Huancané', 'Lampa', 'Livia', 'Mazocruz', 'Pichacani', 'San Antón', 
+               'San José de Salinas', 'San Pedro', 'Santa Rosa'],
+      'Juliaca': ['Juliaca', 'San Román', 'Puno', 'Carabaya', 'Chucuito'],
+      'San Román': ['San Román', 'Juliaca', 'Yunguyo']
+    },
+    'San Martín': {
+      'Moyobamba': ['Moyobamba', 'Bellavista', 'Cacatachi', 'Calzada', 
+                    'Soritor', 'Tabalosos', 'Yantalo'],
+      'Tarapoto': ['Tarapoto', 'Alto Biavo', 'Cacatachi', 'Chazuta', 
+                   'Huallaga', 'Juan Guerra', 'Morales', 'Pucallpa', 
+                   'San Martín'],
+      'Picota': ['Picota', 'Shatoja', 'Tingo de Saposoa']
+    },
+    'Tacna': {
+      'Tacna': ['Tacna', 'Alto de la Alianza', 'Candarave', 'Calana', 
+                'Ciudad Nueva', 'Inclán', 'Pocollay', 'Sama', 'La Yarada'],
+      'Jorge Basadre': ['Jorge Basadre', 'Locumba', 'Santo Domingo', 
+                        'San Pedro', 'La Yarada'],
+      'Tarata': ['Tarata', 'Estique', 'Estique Pampa', 'Locumba', 
+                 'Santo Domingo']
+    },
+    'Tumbes': {
+      'Tumbes': ['Tumbes', 'Cañaveral', 'Corrales', 'La Cruz', 
+                 'Pampa Grande', 'Zorritos'],
+      'Zorritos': ['Zorritos', 'Papayal', 'Pampa Grande']
+    },
+    'Ucayali': {
+      'Pucallpa': ['Pucallpa', 'Yarinacocha', 'Calleria', 'Campoverde', 
+                   'Manantay', 'Nueva Requena', 'Curimana'],
+      'Coronel Portillo': ['Coronel Portillo', 'San Alejandro', 
+                           'José Carlos Mariátegui', 'Fitzcarrald']
+    }
+  
+    };
