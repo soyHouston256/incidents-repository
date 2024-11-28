35547609c2b9e3c3b02b5356008c7983d431d099
Pafer1701 <paoolo3521@gmail.com>
2024-11-28 18:44:05 -0500

    style form

--- src/components/DatePicker.tsx
+++ src/components/DatePicker.tsx
@@ -22,7 +22,7 @@ export function DatePickerReport() {
         <Button
           variant={"outline"}
           className={cn(
-            "w-[280px] justify-start text-left font-normal",
+            "w-[400px] justify-start text-left font-normal",
             !date && "text-muted-foreground"
           )}
         >
