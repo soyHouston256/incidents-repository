10f75e4c2b341a32c27ce47d35572f991efc8bf9
Max Houston Ramirez Martel <8385004+soyHouston256@users.noreply.github.com>
2024-12-02 17:13:13 -0500

    add(api)

--- src/components/LocationForm.tsx
+++ src/components/LocationForm.tsx
@@ -110,7 +110,7 @@ const LocationForm = () => {
     };
    
     try {
-      const response = await fetch('http://localhost:3000/answer', {
+      const response = await fetch('http://http://api.alertape.org/answer', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
